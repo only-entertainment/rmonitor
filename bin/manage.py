0399a6cb017e5495dd81c8e2753d82d579e08cec
Kris Neuharth <kneuharth@truecar.com>
2017-11-26 21:33:04 -0800

    Simplify the run script

--- bin/manage.py
+++ bin/manage.py
@@ -1,34 +1,16 @@
-import cmd
-import sys
-
 from rmonitor.client.client import Client
 from rmonitor.settings.settings import logger
 
 
 def run():
+    """
+    Run and receive messages
+    """
+
     try:
         Client.connect()
     except KeyboardInterrupt:
-            pass
-
-
-class RMonitorShell(cmd.Cmd):
-
-    intro = 'Welcome to the RMonitor shell. Type help or ? to list commands.\n'
-    prompt = '>> '
-
-    def do_q(self, arg):
-        'Exit the system'
-        exit()
-
-    def do_exit(self, arg):
-        'Exit the system'
-        exit()
-
-    def do_run(self, arg):
-        'Run and receive messages'
-        logger.info("Running and monitoring...")
-        run()
+        pass
 
 
 if __name__ == '__main__':
@@ -36,25 +18,5 @@ if __name__ == '__main__':
     # Main Driver
     #
 
-    logger.debug(str(sys.argv))
-    args = sys.argv
-
-    if len(args) > 1:
-        # Get rid of the script name
-        args.pop(0)
-
-        # Get the first arg (run, shell)
-        ccc = args[0]
-
-        if ccc == 'run':
-            RMonitorShell().do_run(None)
-
-        elif ccc == 'shell':
-            RMonitorShell().cmdloop()
-        else:
-            # Default
-            RMonitorShell().cmdloop()
-
-    elif len(args) == 1:
-        # Fully interactive mode
-        RMonitorShell().cmdloop()
+    logger.info("Running and monitoring...")
+    run()
